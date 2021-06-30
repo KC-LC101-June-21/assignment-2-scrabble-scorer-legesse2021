f306ad1e4a3fe32b91dcb59afcfa5e6ada8b7e0d
legesse <75861111+legesse2021@users.noreply.github.com>
2021-06-30 20:47:28 +0000

    finish writing

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -1,7 +1,6 @@
-// inspired by https://exercism.io/tracks/javascript/exercises/etl/solutions/91f99a3cca9548cebe5975d7ebca6a85
-
+// inspired by //https://exercism.io/tracks/javascript/exercises/etl/solutions/91f99a//3cca9548cebe5975d7ebca6a85
 const input = require("readline-sync");
-
+let info = input.question("Enter a word to score: ");
 const oldPointStructure = {
   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
   2: ['D', 'G'],
@@ -11,12 +10,11 @@ const oldPointStructure = {
   8: ['J', 'X'],
   10: ['Q', 'Z']
 };
-
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
  
-	for (let i = 0; i < word.length; i++) {
+	 for (let i = 0; i < word.length; i++) {
  
 	  for (const pointValue in oldPointStructure) {
  
@@ -27,32 +25,101 @@ function oldScrabbleScorer(word) {
 	  }
 	}
 	return letterPoints;
- }
+}
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+    console.clear();
+    console.log("let's play some scrabble!\n").toLowerCase();
+    word = input.question('Enter a word to score: ').toLowerCase();
+     while (!word.match([a-z]  || !word ===isNaN)) {
+       console.clear();
+       word = input.question('INVALID WORD\n\nPlease, this time, enter a real word to score: \n');
+     }
+     console.clear();
 };
 
-let simpleScore;
+
+/*let simpleScore;
 
 let vowelBonusScore;
 
-let scrabbleScore;
+let scrabbleScore;*/
 
 const scoringAlgorithms = [];
+const scoringAlgorithmsimple = {
+    name: "simple Scoring",
+    description: "this function counts all the letters and gives a score of +1 to each letter in the given word.",
+    scoringFunction: simpleScore
+  };
 
-function scorerPrompt() {}
+  const scoringAlgorithmVowelBonus = {
+    name: "Vowel Bonus Scoring",
+    description: "Scores vowels at +3 points, and consonants at +1.",
+    scoringFunction: vowelBonusScore
+  };
+  const scoringAlgorithmScrabble = {
+    name: "Scrabble Point Scoring",
+    description: "Scores each word by individual letter point value based upon original scrabble point values.",
+    scoringFunction: scrabbleScorer
+  };
 
-function transform() {};
+  const scoringAlgorithms = [scoringAlgorithmSimple,scoringAlgorithmVowelBonus, scoringAlgorithmScrabble]
+
+  const oldPointStructure = {
+    0: [' '],
+    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+    2: ['D', 'G'],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+   10: ['Q', 'Z']
+  };
+
+  let newPointStructure = transform(oldPointStructure);
+
+
+
+function scorerPrompt() {
+    console.log("How would you like to score this words?\n");
+    for (i = 0; i < scoringAlgorithms.length; i++) {
+      console.log(`${[i + 1]} ${scoringAlgorithms[i].name}\n${scoringAlgorithms[i].description}\n`);
+    }
+    selection = input.question("Select a choice from above: \n");
+  }
+
+
+function transform() {
+    while ((selection - 1) >= scoringAlgorithms.length || isNaN(selection)) {
+      console.clear();
+      console.log("That is not number or a valid choice from above. Select again:\n")
+       for (i = 0; i < scoringAlgorithms.length; i++) {
+         console.log(`${[i + 1]} ${scoringAlgorithms[i].name}\n`)
+       }
+       selection = input.question("Select a choice from above: \n");
+    }
+    console.clear();
+    return scoringAlgorithms[selection - 1].scoringFunction(word);
+  };
 
 let newPointStructure;
+  function transform(lettersByScore) {
+    const scoreByLetter = {};
+    for (let score in lettersByScore) {
+      let letters = lettersByScore[score];
+      for (let i = 0; i < letters.length; i++) {
+        scoresByLetter[letters[i].toLowercase()] = Number(score)
+      }
+    }
+    return scoreByLetter;
+  };
 
 function runProgram() {
    initialPrompt();
-   
+   scorerPrompt();
 }
 
 // Don't write any code below this line //
